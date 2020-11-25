4461cd0075629b87395385d2c59a966b0f0cfaf0
Marko <Marko3802>
2020-11-25 22:50:07 +0100

    added JPQL sort method

--- src/main/java/ch/zli/m223/punchclock/repository/ApplicationUserRepository.java
+++ src/main/java/ch/zli/m223/punchclock/repository/ApplicationUserRepository.java
@@ -1,8 +1,14 @@
 package ch.zli.m223.punchclock.repository;
 
 import ch.zli.m223.punchclock.domain.ApplicationUser;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
     ApplicationUser findByUsername(String username);
+    @Query(value = "SELECT username FROM User")
+    List<ApplicationUser> findAllUsers(Sort sort);
 }
